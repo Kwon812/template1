af30c8fb4e96e45b39101777566df28db2ba3d99
Kwon812 <tre723@naver.com>
2021-01-01 14:08:11 +0900

    ㅗㅓ

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ async function predict() {
     const probability = prediction[i].probability.toFixed(2) * 100;
 
     const data = {
-      BarredSpiralGalaxy: 'Barred', NormalSpiralGalaxy: 'Normal', IrregularGalaxy:'Irregular',
+      BarredSpiralGalaxy: 'Barred', NormalSpiralGalaxy: "일반", IrregularGalaxy:'Irregular',
       
     };
 
